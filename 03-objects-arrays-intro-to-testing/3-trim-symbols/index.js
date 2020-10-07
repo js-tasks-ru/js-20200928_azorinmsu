65777878056fc8b1a82635814caf479cc8c57819
Aleksandr Zorin <azorinmsu@gmail.com>
2020-10-08 01:33:48 +0300

    3-е занятие

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size <= 0) {
+    return "";
+  }
 
+  const symbols = string.split("");
+
+  let prev = "";
+  let result = "";
+  let count = 1;
+  for (const symbol of symbols) {
+    if (prev === symbol) {
+      count = count + 1;
+      if (size < count) {
+        count = 1;
+        continue;
+      }
+    } else {
+      count = 1;
+    }
+
+    result += symbol;
+    prev = symbol;
+  }
+
+  return result;
 }
